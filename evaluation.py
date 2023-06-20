f1e346edcfce103196dd3a2dbff4438ebc029947
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-06-20 13:30:21 +0200

    moved colour pallete to eval

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,13 @@
-
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+UU_YELLOW = "#FFCD00"
+UU_RED = "#C00A35"
+UU_CREME = "#FFE6AB"
+UU_ORANGE = "#F3965E"
+UU_BURGUNDY = "#AA1555"
+UU_BROWN = "#6E3B23"
+UU_PAL = sns.color_palette([UU_YELLOW, UU_RED, UU_CREME, UU_ORANGE, UU_BURGUNDY, UU_BROWN])  # Make pallete
 
 
 def plot_history(history):
