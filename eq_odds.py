3102b75d8844ebb30481cacee26db5bbd0fbd7aa
Denbergvanthijs <denbergvanthijs@gmail.com>
2023-06-22 18:25:13 +0200

    Cleaned code

--- eq_odds.py
+++ eq_odds.py
@@ -15,74 +15,51 @@ baseline = load_model("./models/baseline.h5")
 # Load data from disk
 fps = ["./data/intersectional_train.csv", "./data/intersectional_val.csv", "./data/intersectional_test.csv"]
 X_train, X_val, X_test, y_train, y_val, y_test = data_preparation(fps=fps)
+labels_intersection = {11: "Married Men", 12: "Single Men", 21: "Married Women", 22: "Single Women"}
 
 # Equalised odds
 # Equal TPR and FPR for each group
 # TPR = TP / (TP + FN) = p[D = 1 | Y = 1 | A = a] for all four intersectional groups a
 # FPR = FP / (FP + TN) = p[D = 1 | Y = 0 | A = a] for all four intersectional groups a
 
-# Calculate current TPR and FPR for each group
-F1 = []
-SP = []  # Statistical parity
-EO = []  # Equal opportunity
-FPR = []  # False positive rate
-y_subsets = []
-y_scores = []
-labels_intersection = {11: "Married Men", 12: "Single Men", 21: "Married Women", 22: "Single Women"}
-for group in (11, 12, 21, 22):
+# Calculate and plot ROC curves for each group
+y_trues = []
+metrics_baseline = {"f1": [], "SP": [], "EO": [], "FPR": []}
+fig, ax = plt.subplots(figsize=(6, 6))
+for c, group in enumerate((11, 12, 21, 22)):
     idx = X_test["intersection"] == group
 
     X_subset = X_test[idx]
-    y_subset = y_test[idx]
-    y_subsets.append(y_subset)
+    y_true = y_test[idx]
+    y_trues.append(y_true)
 
     # Drop intersectional group column
     X_subset = X_subset.drop("intersection", axis=1)
 
     # Predict on subset
     y_pred = baseline.predict(X_subset)
-    y_scores.append(y_pred)
 
     # Calculate metrics for subset
-    metrics = Baseline.get_metrics(y_subset, y_pred, threshold=0.5)
+    metrics = Baseline.get_metrics(y_true, y_pred, threshold=0.5)
+    metrics_baseline["f1"].append(metrics["f1"])  # Fidelity metric
+    metrics_baseline["SP"].append(metrics["SP"])  # Statistical parity
+    metrics_baseline["EO"].append(metrics["EO"])  # Equalised opportunity, same as TPR
+    metrics_baseline["FPR"].append(metrics["FPR"])  # False positive rate
 
-    # Fidelity metrics
-    F1.append(metrics["f1"])
-
-    # Fairness metrics
-    SP.append(metrics["SP"])
-    EO.append(metrics["EO"])  # EO is equal to TPR/Recall
-    FPR.append(metrics["FPR"])
-
-# To numpy array
-F1 = np.array(F1)
-SP = np.array(SP)
-EO = np.array(EO)
-FPR = np.array(FPR)
+    # Calculate ROC curve
+    FPR, TPR, thresholds = sklearn.metrics.roc_curve(y_true, y_pred)
+    ax.plot(FPR, TPR, label=labels_intersection[group], color=UU_PAL[c])  # Plot ROC curve
 
 print("Baseline model metrics for each group:")
 print(list(labels_intersection.values()))
-print(f"F1: {F1.round(2)}")
-print(f"SP: {SP.round(2)}")
-print(f"EO/TPR: {EO.round(2)}")
-print(f"FPR: {FPR.round(2)}")
-
-fig, ax = plt.subplots(figsize=(6, 6))
-# Calculate and plot ROC curves for each group
-tpr_scores = []
-fpr_scores = []
-for c, group in enumerate((11, 12, 21, 22)):
-    fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_subsets[c], y_scores[c])
-    tpr_scores.append(tpr)
-    fpr_scores.append(fpr)
-
-    # Plot ROC curve
-    ax.plot(fpr, tpr, label=labels_intersection[group], color=UU_PAL[c])
+for key in metrics_baseline.keys():
+    metrics_baseline[key] = np.array(metrics_baseline[key])
+    print(f"{key}: {metrics_baseline[key].round(2)}")
 
 # Add overall ROC curve
-X_test_no_inter = X_test.drop("intersection", axis=1)
-fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_test, baseline.predict(X_test_no_inter))
-ax.plot(fpr, tpr, label="Overall", color="black", linestyle="--")
+y_pred = baseline.predict(X_test.drop("intersection", axis=1))  # Predict on all test data
+FPR, TPR, thresholds = sklearn.metrics.roc_curve(y_test, y_pred)
+ax.plot(FPR, TPR, label="Overall", color="black", linestyle="--")
 
 ax.set_aspect("equal", "box")
 plt.xlabel("FPR")
@@ -129,65 +106,40 @@ eq_odds.fit(aif_train, aif_train_pred)  # Fit on original vs predicted labels by
 eq_odds_test = eq_odds.predict(aif_test_pred)
 
 # Calculate new TPR and FPR for each group
-F1_new = []
-SP_new = []  # Statistical parity
-EO_new = []  # Equal opportunity
-FPR_new = []
+metrics_new = {"f1": [], "SP": [], "EO": [], "FPR": []}
+fig, ax = plt.subplots(figsize=(6, 6))
 for c, group in enumerate((11, 12, 21, 22)):
     idx = eq_odds_test.protected_attributes[:, 0] == group
     y_pred = eq_odds_test.labels[idx]
 
     # Calculate metrics for subset
-    metrics = Baseline.get_metrics(y_subsets[c], y_pred, threshold=0.5)
+    metrics = Baseline.get_metrics(y_trues[c], y_pred, threshold=0.5)
+    metrics_new["f1"].append(metrics["f1"])  # Fidelity metric
+    metrics_new["SP"].append(metrics["SP"])  # Statistical parity
+    metrics_new["EO"].append(metrics["EO"])  # Equalised opportunity, same as TPR
+    metrics_new["FPR"].append(metrics["FPR"])  # False positive rate
 
-    # Fidelity metrics
-    F1_new.append(metrics["f1"])
+    # Calculate ROC curve
+    FPR, TPR, thresholds = sklearn.metrics.roc_curve(y_trues[c], y_pred)
+    ax.plot(FPR, TPR, label=labels_intersection[group], color=UU_PAL[c])  # Plot ROC curve
 
-    # Fairness metrics
-    SP_new.append(metrics["SP"])
-    EO_new.append(metrics["EO"])  # EO is equal to TPR/Recall
-    FPR_new.append(metrics["FPR"])
-
-# To numpy array
-F1_new = np.array(F1_new)
-SP_new = np.array(SP_new)
-EO_new = np.array(EO_new)
-FPR_new = np.array(FPR_new)
-
-print("After Equal Odds mitigation metrics for each group:")
+print("After Equalised Odds mitigation metrics for each group:")
 print(list(labels_intersection.values()))
-print(f"F1: {F1.round(2)}")
-print(f"SP: {SP.round(2)}")
-print(f"EO/TPR: {EO.round(2)}")
-print(f"FPR: {FPR.round(2)}")
+for key in metrics_new.keys():
+    metrics_new[key] = np.array(metrics_new[key])
+    print(f"{key}: {metrics_new[key].round(2)}")
 
+weights = [y_trues[c].size / y_test.size for c in range(4)]  # Weights for micro-average
 # Calculate percentual change of metrics, element-wise
-weights = [y_subsets[c].size / y_test.size for c in range(4)]
-for new, old, name in zip((F1_new, SP_new, EO_new, FPR_new), (F1, SP, EO, FPR), ("F1", "SP", "EO", "FPR")):
-    change = (new - old) / old
+for metric, old_value in metrics_baseline.items():
+    change = (metrics_new[metric] - old_value) / old_value  # Percental change
     macro = np.mean(change)
     micro = np.average(change, weights=weights)
-    print(f"Change in {name} scores: {change.round(2)}; Macro-average: {macro.round(2)}; Micro-average: {micro.round(2)}")
-
-# New ROC curve
-fig, ax = plt.subplots(figsize=(6, 6))
-# Calculate and plot ROC curves for each group
-tpr_scores_new = []
-fpr_scores_new = []
-for c, group in enumerate((11, 12, 21, 22)):
-    idx = eq_odds_test.protected_attributes[:, 0] == group
-    y_pred = eq_odds_test.labels[idx]
-
-    fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_subsets[c], y_pred)
-    tpr_scores_new.append(tpr)
-    fpr_scores_new.append(fpr)
-
-    # Plot ROC curve
-    ax.plot(fpr, tpr, label=labels_intersection[group], color=UU_PAL[c])
+    print(f"Change in {metric} scores: {change.round(2)}; Macro-average: {macro.round(2)}; Micro-average: {micro.round(2)}")
 
 # Add overall ROC curve
-fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_test, eq_odds_test.labels)
-ax.plot(fpr, tpr, label="Overall", color="black", linestyle="--")
+FPR, TPR, thresholds = sklearn.metrics.roc_curve(y_test, eq_odds_test.labels)
+ax.plot(FPR, TPR, label="Overall", color="black", linestyle="--")
 
 ax.set_aspect("equal", "box")
 plt.xlabel("FPR")
